6771b8c1fdf33c962544399b2a8001eff7c09064
VinodNaidu <vinoddasari3124@gmail.com>
2025-03-27 07:25:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let display = document.getElementById("display");
+function appendValue(value) {
+    if (display.innerText === "0") {
+        display.innerText = value;
+    } else {
+        display.innerText += value;
+    }
+}
+function clearDisplay() {
+    display.innerText = "0";
+}
+function calculateResult() {
+    try {
+        display.innerText = eval(display.innerText);
+    } catch {
+        display.innerText = "Error";
+    }
+}
